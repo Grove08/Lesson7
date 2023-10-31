dd944b17f548d44a62ccf67f40541a16de3a1c78
Артём Белоусов <belousov@psksk.ru>
2023-10-31 11:05:26 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static double CalculateTriangleArea(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double area1 = CalculateTriangleArea(x1, y1, z1);
+
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            double area2 = CalculateTriangleArea(x2, y2, z2);
+
+            if (area1 > area2)
+                Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if (area1 < area2)
+                Console.WriteLine("Площаль второго треугольника больше");
+            else
+                Console.WriteLine("Площади равны");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void GetParamsCube(double a, out double s, out double v)
+        {
+            s = 6 * a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsCube(a, out s, out v);
+            Console.WriteLine("Площадь поверхности куба: {0:.00}", s);
+            Console.WriteLine("Объем куба: {0:.00}", v);
+            Console.ReadKey();
+        }
+    }
+}
